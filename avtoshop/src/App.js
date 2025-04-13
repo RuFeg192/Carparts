88676eba49f01fb147a8e80dfc88f6aa65b161ef
Mark <karcevskijmark1@gmail.com>
2025-04-13 18:41:11 +0300

    first commit

---
+++ avtoshop/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
